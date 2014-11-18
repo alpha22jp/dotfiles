f23e998722d29e318de7a29f891ee503dc4b9276
alpha22jp <alpha22jp@gmail.com>
2014-11-18 18:02:24 +0900

    marmalade はデフォルトでは読み込まないように変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,7 @@
 (when (locate-library "package")
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   ;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (package-initialize))
 
