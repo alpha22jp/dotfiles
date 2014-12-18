0309c6f0764a537ebc79d62c02905bf37763c432
alpha22jp <alpha22jp@gmail.com>
2014-12-18 11:38:33 +0900

    autoinsertの設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,6 +82,34 @@
 (when (<= emacs-major-version 23)
   (require 'diff-color nil t))
 
+;; autoinsert
+;;
+(when (locate-library "autoinsert")
+  (require 'autoinsert)
+  (auto-insert-mode)
+  (custom-set-variables
+   '(auto-insert-query t)
+   '(auto-insert-alist
+     '((("\\.\\(c\\|cpp\\)$" . "C/C++ source") . ["template.c" my:template-insert-template])
+       (("\\.\\(h\\|hpp\\)$" . "C/C++ header") . ["template.h" my:template-insert-template])))
+   '(auto-insert-directory "~/.emacs.d/template/"))
+  (defun my:template-get-filename-base ()
+    (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
+  (defun my:template-get-filename-ext ()
+    (file-name-extension (file-name-nondirectory (buffer-file-name))))
+  (defvar my:template-replacement-alist
+    '(("%file%"          . (lambda () (file-name-nondirectory (buffer-file-name))))
+      ("%date%"          . (lambda () (format-time-string "%Y-%m-%d")))
+      ("%include-guard%" . (lambda () (format "__%s_%s__"
+                                              (upcase (my:template-get-filename-base))
+                                              (upcase (my:template-get-filename-ext)))))))
+  (defun my:template-insert-template ()
+    (mapc '(lambda (c)
+             (progn
+               (replace-string (car c) (funcall (cdr c)) nil)
+               (goto-char (point-min))))
+          my:template-replacement-alist)))
+
 ;; flymake
 ;;
 (when (locate-library "flymake")
