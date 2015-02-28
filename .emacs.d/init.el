36cbf3e27b4f89f9c6f8aedaa1ec00f4ed898e12
alpha22jp <alpha22jp@gmail.com>
2015-02-28 20:51:42 +0900

    markdown-modeのauto-mode-alist設定を追加
    Simplenoteの編集をブラウザからIt's All Text経由で行うとき、デフォルトで
    markdown-modeになるようにする。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -302,7 +302,8 @@
 (when (locate-library "markdown-mode")
   (autoload 'markdown-mode "markdown-mode"
     "Major mode for editing Markdown files" t)
-  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode)))
+  (add-to-list 'auto-mode-alist
+               '("\\.md\\'\\|app\\.simplenote\\.com_" . markdown-mode)))
 
 ;; simplenote2
 ;;
