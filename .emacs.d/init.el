63f1432361fcd23f374e4291bd4f8bf6b81f464a
alpha22jp <alpha22jp@gmail.com>
2015-03-05 23:36:32 +0900

    helm-gtagsの設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -352,6 +352,21 @@
     '(progn
        (define-key helm-find-files-map (kbd "C-i") 'helm-execute-persistent-action))))
 
+;; helm-gtags
+;;
+(require 'helm-gtags)
+(add-hook 'c-mode-hook (lambda () (helm-gtags-mode)))
+;; customize
+(setq helm-c-gtags-path-style 'relative)
+(setq helm-c-gtags-ignore-case t)
+;; key bindings
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-.") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-@") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "M-,") 'helm-gtags-pop-stack)))
+
 ;; wgrep-ag
 ;;
 (when (locate-library "ag")
