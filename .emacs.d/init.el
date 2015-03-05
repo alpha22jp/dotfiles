4eff17d3884da3d076c78889181cae9deca3c59d
alpha22jp <alpha22jp@gmail.com>
2015-03-06 00:27:51 +0900

    helm-gtagsがないときにエラーになるのを修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -354,18 +354,19 @@
 
 ;; helm-gtags
 ;;
-(require 'helm-gtags)
-(add-hook 'c-mode-hook (lambda () (helm-gtags-mode)))
-;; customize
-(setq helm-c-gtags-path-style 'relative)
-(setq helm-c-gtags-ignore-case t)
-;; key bindings
-(add-hook 'helm-gtags-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "M-.") 'helm-gtags-find-tag)
-             (local-set-key (kbd "M-@") 'helm-gtags-find-rtag)
-             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-             (local-set-key (kbd "M-,") 'helm-gtags-pop-stack)))
+(when (locate-library "helm-gtags")
+  (require 'helm-gtags)
+  (add-hook 'c-mode-hook (lambda () (helm-gtags-mode)))
+  ;; customize
+  (setq helm-c-gtags-path-style 'relative)
+  (setq helm-c-gtags-ignore-case t)
+  ;; key bindings
+  (add-hook 'helm-gtags-mode-hook
+            '(lambda ()
+               (local-set-key (kbd "M-.") 'helm-gtags-find-tag)
+               (local-set-key (kbd "M-@") 'helm-gtags-find-rtag)
+               (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+               (local-set-key (kbd "M-,") 'helm-gtags-pop-stack))))
 
 ;; wgrep-ag
 ;;
