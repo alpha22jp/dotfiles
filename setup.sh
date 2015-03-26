2f6cad0fbff12a71de9008509eaf516d14b4d74f
alpha22jp <alpha22jp@gmail.com>
2015-03-26 10:22:48 +0900

    .emacs.d/lisp/ディレクトリを丸ごとsymbolic linkするように変更
    それに伴い、Emacs23だけで必要になるファイルはlisp-23/に移動

--- setup.sh
+++ setup.sh
@@ -35,34 +35,13 @@ done
 #-----------------------------------------------------------------------
 # Emacsの設定ファイル
 #-----------------------------------------------------------------------
-emacs_files=(
-    "my-package-list.el"
-    "my-auto-insert.el"
-    "my-proxy-toggle.el"
-    "diff-color.el"
-    "elscreen.el"
-    "fuzzy-format.el"
-    "isearch-region.el"
-    "flymake-popup.el"
-    "psvn.el"
-)
-emacs23_files=(
-    "flymake-fringe.el"
-)
 
 if [ `which emacs` ]; then
     emacs_version=`emacs --version | sed '2,$d; s/.* \([0-9]*\).*/\1/'`
     if [ $emacs_version -ge 23 ]; then
-        mkdir -p .emacs.d/lisp
+        ln -sf {~/dotfiles/,}.emacs.d/lisp
+        ln -sf {~/dotfiles/,}.emacs.d/lisp-23
         symlink_to_dotfiles ".emacs.d/init.el"
-        for file in ${emacs_files[@]}; do
-            symlink_to_dotfiles ".emacs.d/lisp/$file"
-        done
-        if [ $emacs_version -lt 24 ]; then
-            for file in ${emacs23_files[@]}; do
-                symlink_to_dotfiles ".emacs.d/lisp/$file"
-            done
-        fi
     else
         echo "Emacs version should be greater than 23"
     fi
