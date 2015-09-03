24bbcc96f3d1069c59b3e730dfa51726068e76d0
alpha22jp <alpha22jp@gmail.com>
2015-09-03 21:45:37 +0900

    編集中のバッファのファイル名を変更するelispを追加

---
+++ .emacs.d/lisp/rename-file-buffer.el
@@ -0,0 +1,44 @@
+;;; rename-file-buffer.el --- Rename file name visiting on current buffer  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  alpha22jp
+
+;; Author: alpha22jp <alpha22jp@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+(provide 'rename-file-buffer)
+;;; rename-file-buffer.el ends here
