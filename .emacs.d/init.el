a613130c0a9ccad78785a15fbc7c78f5d58f3ad2
alpha22jp <alpha22jp@gmail.com>
2015-12-20 10:19:41 +0900

    yasnippetとcompanyのtabキー競合対策

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,6 +130,8 @@
 ;;
 (eval-after-load "yasnippet"
   '(progn
+     ;; companyと競合するのでyasnippetのフィールド移動は "C-i" のみにする
+     (define-key yas-keymap (kbd "<tab>") nil)
      (yas-global-mode 1)))
 
 ;; smartparens
