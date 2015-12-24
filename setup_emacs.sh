d481d635fc8c7687b17f509680b0392fac7f9cb2
alpha22jp <alpha22jp@gmail.com>
2015-12-24 22:21:36 +0900

    lisp-localディレクトリを作成するように修正

--- setup_emacs.sh
+++ setup_emacs.sh
@@ -8,7 +8,7 @@ base_dir=$(cd $(dirname $0); pwd)
 if [ `which emacs` ]; then
     emacs_version=`emacs --version | sed '2,$d; s/.* \([0-9]*\).*/\1/'`
     if [ $emacs_version -ge 24 ]; then
-        mkdir -p ~/.emacs.d
+        mkdir -p ~/.emacs.d/lisp-local
         ln -sf {$base_dir,~}/.emacs.d/init.el
         ln -sf {$base_dir,~}/.emacs.d/lisp
         ln -sf {$base_dir,~}/.emacs.d/snippets
