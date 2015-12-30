d0516e83b3c9b47177fdb4c761062b9ccf58c436
alpha22jp <alpha22jp@gmail.com>
2015-12-30 15:03:48 +0900

    irony-modeの設定を改善

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -210,17 +210,11 @@
 ;;
 (eval-after-load "irony"
   '(progn
-     (add-hook 'irony-mode-hook
-               (lambda ()
-                 (setq irony-additional-clang-options '("-std=c++11"))
-                 (irony-cdb-autosetup-compile-options)))
+     (custom-set-variables '(irony-additional-clang-options '("-std=c++11")))
+     (add-to-list 'company-backends 'company-irony)
+     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
      (add-hook 'c++-mode-hook 'irony-mode)
      (add-hook 'c-mode-hook 'irony-mode)))
-
-;; company-irony
-;;
-(eval-after-load "company"
-  '(add-to-list 'company-backends 'company-irony))
 
 ;; rtags
 ;;
