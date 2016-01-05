52012e992e53f96736990d9f1ddb2921a2c92525
alpha22jp <alpha22jp@gmail.com>
2016-01-05 20:33:57 +0900

    MAC向けフォントのサイズ設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,13 +25,16 @@
 
 ;; font settings
 ;;
-(defvar my-default-font "Ricty Diminished")
+(defvar my-default-font-family "Ricty Diminished")
+(defvar my-default-font-size (if (eq system-type 'darwin) '160 '110))
 (defun my-font-setting ()
   "My customized font setting function."
-  (set-face-attribute 'default nil :family my-default-font :height 120)
+  (set-face-attribute 'default nil
+                      :family my-default-font-family
+                      :height my-default-font-size)
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    (font-spec :family my-default-font :size 16)))
+                    (font-spec :family my-default-font-family :size 16)))
 (unless (string= (frame-parameter nil 'font) "tty") (my-font-setting))
 
 ;; keyboard-translate settings
