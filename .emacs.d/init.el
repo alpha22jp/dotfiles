e89b6eb5ada01385a0762193a62079a12da58267
alpha22jp <alpha22jp@gmail.com>
2016-01-09 10:28:51 +0900

    LinuxではasciiのみMigu 2Mを使うように変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,8 +26,8 @@
 ;; font settings
 ;;
 (defvar my-default-font-family
-  (if (eq system-type 'darwin) "Ricty Diminished" "Ricty Diminished"))
-(defvar my-default-font-size (if (eq system-type 'darwin) '16 '12))
+  (if (eq system-type 'darwin) "Ricty Diminished" "Migu 2M"))
+(defvar my-default-font-size (if (eq system-type 'darwin) '16 '11))
 (defun my-font-setting ()
   "My customized font setting function."
   (set-face-attribute 'default nil :font
@@ -35,7 +35,7 @@
                               (number-to-string my-default-font-size)))
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    (font-spec :family my-default-font-family :size 16)))
+                    (font-spec :family "Ricty Diminished" :size 16)))
 (unless (string= (frame-parameter nil 'font) "tty") (my-font-setting))
 
 ;; keyboard-translate settings
