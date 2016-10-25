f72fa99d7600d24d5ce2caec5d0834160f4aa68e
alpha22jp <alpha22jp@gmail.com>
2016-10-25 22:37:39 +0900

    edit-serverを削除してatomic-chromeに変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -132,6 +132,10 @@
      (setq recentf-exclude '("/.emacs.d/elpa/*" "/.simplenote2/*" "/TAGS$" "/COMMIT_EDITMSG$"))
      (use-package recentf-ext)))
 
+;; atomic-chrome
+;;
+(use-package atomic-chrome)
+
 ;; autoinsert
 ;;
 (use-package autoinsert
@@ -461,13 +465,6 @@
      (:cmdopt . "--harmony")))
   (quickrun-set-default "javascript" "javascript/node-harmony"))
 
-;; edit server (for editing chrome text area)
-;;
-(use-package edit-server
-  :config
-  (setq edit-server-new-frame nil)
-  (edit-server-start))
-
 ;; c/c++ mode
 ;;
 (eval-after-load "cc-mode"
@@ -595,7 +592,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (edit-server wgrep-ag use-package swift-mode solarized-theme smartparens smart-mode-line simplenote2 rtags region-bindings-mode recentf-ext quickrun popup mozc mc-extras markdown-mode magit json-mode js2-mode helm-swoop helm-gtags helm-descbinds helm-cscope helm-c-yasnippet helm-ag google-c-style git-gutter-fringe flycheck-irony expand-region exec-path-from-shell company-irony cmake-mode ag haskell-mode powerline))))
+    (atomic-chrome wgrep-ag use-package swift-mode solarized-theme smartparens smart-mode-line simplenote2 rtags region-bindings-mode recentf-ext quickrun popup mozc mc-extras markdown-mode magit json-mode js2-mode helm-swoop helm-gtags helm-descbinds helm-cscope helm-c-yasnippet helm-ag google-c-style git-gutter-fringe flycheck-irony expand-region exec-path-from-shell company-irony cmake-mode ag haskell-mode powerline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
