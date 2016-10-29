16e7f7ad502b7cacba58ce2344a74fab4f59ea40
alpha22jp <alpha22jp@gmail.com>
2016-10-29 17:46:20 +0900

    auto-revert-modeをモードラインに表示しないように設定

--- .emacs.d/lisp/my-mode-line.el
+++ .emacs.d/lisp/my-mode-line.el
@@ -9,17 +9,15 @@
 
 (defvar mode-line-cleaner-alist
   '((abbrev-mode . "")
+    (auto-revert-mode . "")
     (emacs-lisp-mode . "Elisp")))
 
 (defun clean-mode-line ()
   "Clean mode line."
   (loop for (mode . mode-str) in mode-line-cleaner-alist do
         (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
-          (when old-mode-str
-            (setcar old-mode-str mode-str))
-          ;; major mode
-          (when (eq mode major-mode)
-            (setq mode-name mode-str)))))
+          (when old-mode-str (setcar old-mode-str mode-str))
+          (when (eq mode major-mode) (setq mode-name mode-str)))))
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
