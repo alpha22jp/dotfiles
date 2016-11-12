ce67291b6f72a8f25cdd25c3f6b9cde8d748f7b1
alpha22jp <alpha22jp@gmail.com>
2016-11-12 14:38:08 +0900

    javaファイルを開いたときにirony-modeの警告が出るのを修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -180,7 +180,8 @@
   (setq irony-additional-clang-options '("-std=c++11"))
   (add-to-list 'company-backends 'company-irony)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-  (add-hook 'c-mode-common-hook 'irony-mode))
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'c++-mode-hook 'irony-moe))
 
 ;; flycheck
 ;;
